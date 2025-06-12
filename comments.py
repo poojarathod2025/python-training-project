e9f2e8619b34793f7dd0094b9c186c825b0ddfd9
poojarathod2025 <poojarathodpr21@gmail.com>
2025-06-12 22:08:49 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,9 @@
+comments starts with #
+we can use ctrl / for giving comment
+it provides code information for easy readability
+it shows information for whole code
+for reading comment
+
+docstrings
+we can provide it by using ''' ''' or """" """
+it is only for giving information of function or class
