fd485b347cad70b5fe142fc8653ae134eee2393d
pooja rathod <poojarathodpr21@gmail.com>
2025-06-18 17:28:47 +0530

    if else.py

---
+++ if_elif_else.py
@@ -0,0 +1,31 @@
+if else
+if-when we use if if multiple times in code,it will run after condition satiesfied
+elif-it will not run next conditions
+e.g.
+# a = 10
+# if a ==10:
+#     print(True)
+# if a ==9:
+#     print(False)
+# else:
+#     print("hello")
+
+# a = 9
+# if a ==10:
+#     print(True)
+# elif a ==9:
+#     print(False)
+# elif a ==8:
+#     print(False)
+# else:
+#     print("hello")
+
+a = int(input("please enter score:"))
+  if a>=90:
+    print("grade a")
+  elif a>=80:
+    print("grade b")
+  elif a>=70:
+    print("grade c")
+  else:
+    print("fail")
